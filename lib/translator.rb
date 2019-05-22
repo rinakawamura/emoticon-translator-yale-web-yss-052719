abe8da1b0582a54b06a2cf1185dbdc8930ccbf30
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-22 17:41:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,22 +4,24 @@ def load_library(path)
   emoticons = YAML.load_file(path)
 end
 
-def get_japanese_emoticon(western)
-  japanese = nil
-  emoticons.each do |name, symbols|
-    if symbols[0] == western
-      japanese = symbols[1]
-    end
-  end
-  return japanese
-end
+load_library(./lib/emoticons.yml)
 
-def get_english_meaning(japanese)
-  western = nil
-  emoticons.each do |name, symbols|
-    if symbols[1] == japanese
-      western = symbols[0]
-    end
-  end
-  return western
-end
+# def get_japanese_emoticon(western)
+#   japanese = nil
+#   emoticons.each do |name, symbols|
+#     if symbols[0] == western
+#       japanese = symbols[1]
+#     end
+#   end
+#   return japanese
+# end
+#
+# def get_english_meaning(japanese)
+#   western = nil
+#   emoticons.each do |name, symbols|
+#     if symbols[1] == japanese
+#       western = symbols[0]
+#     end
+#   end
+#   return western
+# end
